ef54171f33909980177cb8004c49f5bc98740a94
Marie-Sail <marie.saillard.pro@gmail.com>
2023-11-23 17:59:53 +0100

    composant baubles

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,14 +1,20 @@
 import "./Homepage.scss";
+import Baubles from "../../components/baubles/Baubles";
 
 function Homepage() {
   return (
-    <section className="homepage">
+    <main className="homepage">
+      <section>
+        <Baubles/>
+      </section>
+      <section>
       <h1 className="homepage__title">
         Tu les veux les meilleurs excuses pour ne pas fêter Noël ??
       </h1>
       {/*cool button*/}
       <button className="homepage__button">Click ici !</button>
-    </section>
+      </section>
+    </main>
   );
 }
 
