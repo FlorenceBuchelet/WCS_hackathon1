506b9775182297ad4d0bd2a7eac138c4d2f8c4e9
Florence Buchelet <florence.buchelet@gmail.com>
2023-11-23 22:12:57 +0100

    question 1 style

--- src/components/question/question1/question1.jsx
+++ src/components/question/question1/question1.jsx
@@ -10,9 +10,14 @@ function Question1() {
   };
   return (
     <div className="question1">
+        <h1 className="question1__title">Mais en es-tu digne ?</h1>
+          <img
+            className="question1__image"
+            src="../../assets/adoubement.jpg"
+            alt="tableau d'un adoubement"
+          />
       <section className="question1__section">
-        <h1>En es-tu digne ?</h1>
-        <button className="question1__button">
+        <button className="question1__buttons">
           <Link
             activeClass="active"
             to="question2"
@@ -21,10 +26,10 @@ function Question1() {
             offset={0}
             duration={5000}
             onClick={handleclick}>
-            Oui
+            Évidemment
           </Link>
         </button>
-        <button className="question1__button">
+        <button className="question1__buttons">
           <Link
             activeClass="active"
             to=""
@@ -33,14 +38,9 @@ function Question1() {
             offset={0}
             duration={5000}
             onClick={handleclick}>
-            non
+            Nope, vive Noël !!
           </Link></button>
       </section>
-      <img
-        className="question1__image"
-        src="../../assets/adoubement.jpg"
-        alt="tableau d'un adoubement"
-      />
     </div>
   );
 }
--- src/pages/Questions/Questions.jsx
+++ src/pages/Questions/Questions.jsx
@@ -1,19 +1,20 @@
-import Question1 from "./components/question/question1/question1";
-import Question2 from "./components/question/question2/question2";
-import RobotQuestion from "./components/question/question3/RobotQuestion";
-import Question4 from "./components/question/question4/question4";
-import Question5 from "./components/question/question5/question5";
-import Question6 from "./components/question/question6/question6";
-import Question7 from "./components/question/question7/question7";
-import Question8 from "./components/question/question8/question8";
-import Question9 from "./components/question/question9/question9";
+import Question1 from "../../components/question/question1/question1";
+import Question2 from "../../components/question/question2/question2";
+import RobotQuestion from "../../components/question/question3/RobotQuestion";
+import Question4 from "../../components/question/question4/question4";
+import Question5 from "../../components/question/question5/question5";
+import Question6 from "../../components/question/question6/question6";
+import Question7 from "../../components/question/question7/question7";
+import Question8 from "../../components/question/question8/question8";
+import Question9 from "../../components/question/question9/question9";
+
 import "./Questions.scss";
 
 function Questions() {
   return (
     <div className="questions">
       <aside className="questions__left">
-        <h1 className="questions__title">Questions</h1>
+        <h1 className="questions__title"></h1>
       </aside>
       <aside className="questions__right">
         <Question1 />
@@ -31,3 +32,4 @@ function Questions() {
 }
 
 export default Questions;
+
