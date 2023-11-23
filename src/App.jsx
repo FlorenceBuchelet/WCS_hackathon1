d042536897a1d885fab0ce589c6df2064697ce52
Florence Buchelet <florence.buchelet@gmail.com>
2023-11-23 19:23:46 +0100

    style Questions.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,13 @@ import Question1 from "./components/question/question 1/question1";
 import Question2 from "./components/question/question2/question2";
 import RobotQuestion from "./components/question/question3/RobotQuestion";
 import Homepage from "./pages/Homepage/Homepage";
+import Questions from "./pages/Questions/Questions";
 
 
 function App() {
   return (
     <>
-    <Homepage />
-{/*       <Question1 />
-      <Question2 />
-      <RobotQuestion /> */}
+      <Questions />
     </>
   );
 }
--- src/pages/Questions/Questions.jsx
+++ src/pages/Questions/Questions.jsx
@@ -1,12 +1,17 @@
-import RobotQuestion from "../../components/RobotQuestion/RobotQuestion";
-import YesNoQuestions from "../../components/YesNoQuestions/YesNoQuestions";
+/* import YesNoQuestions from "../../components/YesNoQuestions/YesNoQuestions"; */
+import RobotQuestion from "../../components/question/question3/RobotQuestion";
+/* import Question1 from "../../components/question/question 1/question1"; */
 import "./Questions.scss";
 
 function Questions() {
   return (
     <div className="questions">
-      <h1 className="questions__title">Questions</h1>
+      <aside className="questions__left">
+        <h1 className="questions__title">Questions</h1>
+      </aside>
+      <aside className="questions__right">
         <RobotQuestion />
+      </aside>
     </div>
   );
 }
