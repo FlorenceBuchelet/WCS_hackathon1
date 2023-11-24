fb45efaaf866f054ffc59b130950ef9318fb9a31
Marie-Sail <marie.saillard.pro@gmail.com>
2023-11-24 01:19:01 +0100

    ajout texture

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.scss";
-import WrongAnswerPopup from "./components/WrongAnswerPopup/WrongAnswerPopup";
+// import WrongAnswerPopup from "./components/WrongAnswerPopup/WrongAnswerPopup";
 /* import Question1 from "./components/question/question1/question1";
 import Question2 from "./components/question/question2/question2";
 import RobotQuestion from "./components/question/question3/RobotQuestion";
