f40eb9b939f7772dcea5ff5e2196265801180b5b
ROBIN FANTINO <robin.fantino.rf@gmail.com>
2023-11-24 11:22:01 +0100

    excuse done

--- src/components/excuse/Excuse.jsx
+++ src/components/excuse/Excuse.jsx
@@ -3,15 +3,10 @@ import "./Excuse.scss";
 function Excuse() {
   return (
     <section className="excuse">
-      <h1 className="excuse__h1">
-        Nos 10 meilleurs techniques pour éviter les fêtes familles.
-      </h1>
+      <h1 className="excuse__h1">Nos ultimes techniques pour éviter Noël.</h1>
       {sorryArry.map((sorry) => (
         <div key={sorry.index}>
-          <h3 className="excuse__h3">
-            {sorry.id}. {sorry.excuse}
-          </h3>
-          <p className="excuse__explication">{sorry.explication}</p>
+          <h3 className="excuse__h3">{sorry.excuse}</h3>
         </div>
       ))}
     </section>
--- src/components/questionAndSorry/sorryArray.jsx
+++ src/components/questionAndSorry/sorryArray.jsx
@@ -7,9 +7,9 @@ const sorryArray = [
   },
   {
     id: 2,
-    excuse: "Je fais une diète paléolithique",
+    excuse: "Dire que tu t'es trompé de date",
     explication:
-      "Assez certaine que le lait de poule et la cipaille n’existaient pas à l’époque de nos ancêtres chasseurs-cueilleurs. Victoire.",
+      "Attends, mais Noël n’est pas censé tomber le quatrième dimanche de décembre après la pleine Lune ? Parce que moi, avec ces histoires de Pâques et de machin, je me perds ! ",
   },
   {
     id: 3,
@@ -31,36 +31,6 @@ const sorryArray = [
       "Dire que tu vas chez la belle famille, et dire à la belle famille que tu vas dans ta famille",
     explication:
       "Attention cependant à ce que lesdites familles ne passent pas Noël ensemble, sinon cette technique aura un effet plutôt limité. Qui plus est, ta famille, mise au courant du fait que tu cherches à les fuir, pourrait te déshériter, et on a vu comment ça se passe chez les Hallyday, c’est très mauvais bail.",
-  },
-  {
-    id: 6,
-    excuse: "Se faire porter pâle",
-    explication:
-      "Cela marche bien pour le travail, cela devrait aussi marcher pour Noël. Tu devras trouver une maladie crédible et grave qui leur fera peur, un truc du genre « j’ai fait une indigestion d’esprit de Noël suite à une surexposition au tube de Mariah Carey, depuis, je ne peux pas me retrouver à proximité de sapins ou de cadeaux sans être pris de violentes convulsions ».",
-  },
-  {
-    id: 7,
-    excuse: "Louper son transport",
-    explication:
-      "Ne marche que si ta famille habite dans un coin reculé de ton habitat naturel, nécessitant l’usage d’un train ou d’un covoiturage pour s’y rendre. Si tu habites dans la même ville, tu pourras toujours prétexter avoir loupé le métro ou ton réveil mais on est pas sûr que cela marche.",
-  },
-  {
-    id: 8,
-    excuse: "Dire que tu t'es trompé de date",
-    explication:
-      "« Attends, mais Noël n’est pas censé tomber le quatrième dimanche de décembre après la pleine Lune ? Parce que moi, avec ces histoires de Pâques et de machin, je me perds ! »",
-  },
-  {
-    id: 9,
-    excuse: "Déclencher l'engueulade du siècle",
-    explication:
-      "L’esprit de Noël, c’est avant tout une bonne dispute intra-familiale autour de la dinde aux marrons. Pourquoi ne pas provoquer celle-ci en amont ? Les tensions étant déjà ravivées, on te fera comprendre qu’il n’est pas nécessaire que tu viennes à Noël. N’hésite pas à dire des choses horribles, comme « j’ai voté Hitler en 33 » ou « je mange du beurre doux ».",
-  },
-  {
-    id: 10,
-    excuse: "Disparaître tel le positif sur mon compte en banque",
-    explication:
-      "Tout simplement ! Tu devras ne plus donner signe de vie pendant tout le mois de décembre, prendre tes cliques et tes claques pour t’exiler dans un endroit où personne ne pourra te retrouver, loin de toute civilisation, sans réseau ni téléphonie, c’est-à-dire Bar-le-Duc ou n’importe quelle autre ville de Lorraine. Tu pourras réapparaître dès le premier janvier telle une petite fleur de printemps !",
   },
 ];
 
