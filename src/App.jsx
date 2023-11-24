3761ec018a9d633cfa2d18f415578ccdf6752ff9
ROBIN FANTINO <robin.fantino.rf@gmail.com>
2023-11-24 13:50:20 +0100

    On a de la neige

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.scss";
 import Questions from "./pages/Questions/Questions";
 import Homepage from "./pages/Homepage/Homepage";
+import "./components/neige/Neige";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <section className="App__question">
         <Questions />
       </section>
+      <div className="s-large"></div>
+      <div className="s-small"></div>
     </>
   );
 }
--- src/components/neige/Neige.jsx
+++ src/components/neige/Neige.jsx
@@ -2,8 +2,7 @@ import "./Neige.scss";
 function Neige() {
   return (
     <div className="snow">
-      <div className="s-large"></div>
-      <div className="s-small"></div>
+
     </div>
   );
 }
