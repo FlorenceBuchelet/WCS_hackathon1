a0932bd4336837f1b37acc637b18b3882ee00e3a
Marie-Sail <marie.saillard.pro@gmail.com>
2023-11-24 13:37:05 +0100

    fin

--- src/components/excuse/Excuse.jsx
+++ src/components/excuse/Excuse.jsx
@@ -3,15 +3,15 @@ import "./Excuse.scss";
 function Excuse() {
   return (
     <section className="excuse">
-      <h1 className="excuse__h1"> Bravo !!! </h1>
-      <h1 className="excuse__h1">
-        Vous en êtes vraiment dignes ! Voici pour vous nos ultimes techniques
-        pour éviter Noël.
-      </h1>
+      <h1 className="excuse__h1"> ✨ Félicitations ✨ </h1>
+      <h2 className="excuse__h2">
+  Nos ultimes techniques
+        pour éviter Noël :
+      </h2>
       <article className="excuse__E">
       {sorryArry.map((sorry) => (
         <div key="excuse__h3">
-          <h3 className={"excuse__h3"} id={`${sorry.index}`}>{`${sorry.index}- `}{sorry.excuse}</h3>
+          <h3 className={"excuse__h3"} id={`${sorry.index}`}><span>{`${sorry.id}.`}</span>{" "}{sorry.excuse}</h3>
         </div>
         
       ))}
